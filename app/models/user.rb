46c63dadc08c6af9422ff8600855c3df28a3a543
Vladislav <lvo123ua@gmail.com>
2018-08-17 14:56:11 +0300

    fix validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: { case_sensitive: false }
-  validates_associated :chats, length: { maximum: 10 }
   has_one :auth_token, dependent: :destroy
   has_many :user_chats, dependent: :destroy
   has_many :messages
--- app/models/user_chat.rb
+++ app/models/user_chat.rb
@@ -1,4 +1,10 @@
 class UserChat < ApplicationRecord
   belongs_to :chat
   belongs_to :user
+
+  validate :validate_user_chats_count
+
+  def validate_user_chats_count
+    errors.add(:user, :invalid) if user.chats.count > 9
+  end
 end
