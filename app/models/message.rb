a2f7f5588b2478698c2a4f0168bceced807d3412
Vladislav <lvo123ua@gmail.com>
2018-08-27 20:28:07 +0300

    tiny changes

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,7 @@ class Message < ApplicationRecord
   belongs_to :user
 
   validates :body, length: { in: 3..255 }
+  validates :user_name, presence: true
 
   delegate :name, to: :user, prefix: 'user'
 end
